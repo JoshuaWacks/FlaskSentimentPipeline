f915c36c3eccad9c73c307bb42c24fe6baec167b
Joshua Wacks <2143116@students.wits.ac.za>
2023-10-19 22:59:17 +0200

    Sentiment Analysis and Checks for profanity

--- endpoint.py
+++ endpoint.py
@@ -1,25 +1,42 @@
 from flask import Flask, jsonify, request 
 from flask_restful import Resource, Api 
+from transformers import pipeline
+from profanity_check import predict, predict_prob
+import json
 
 app = Flask(__name__) 
 api = Api(app)
 
-class sentiment():
-    def __init__(self,text):
-        self.text = text
+# TODO: Some Ideas for improvemts
+# Allow for different models, based on different params
+# Return all model results based on params
+# Preprocessing, check for any unsafe words
+
+# Would be cool to dockerize
+# Host endpoint on free hosting service or Azure?
 
 class endpoint(Resource):
-    
-    def get(self):
-        return jsonify({'message': 'I\'m up'}) 
-    
-    def post(self):
+	def __init__(self):
+		self.classification = pipeline('sentiment-analysis',model="distilbert-base-uncased-finetuned-sst-2-english")
         
-        text = request.get_json()
-        response = jsonify({'sentiment': text})
-        response.status_code = 200
-        
-        return response
+	def get(self):
+		return jsonify({'message': 'I\'m up'}) 
+
+	def post(self):
+		
+		data = request.data
+		data = json.loads(data)
+		input_text = data["text"]
+		prob_contains_profanity = predict_prob([input_text])
+  
+		if prob_contains_profanity > 0.9:
+			response = jsonify({'valid_input':'No','reason':'Input contains profanity','sentiment': 'NA'})
+		else:
+			sentiment = self.classification(input_text)
+			response = jsonify({'valid_input':'Yes','reason':'NA','sentiment': sentiment})
+   
+		response.status_code = 200
+		return response
 
 api.add_resource(endpoint,'/')
 
