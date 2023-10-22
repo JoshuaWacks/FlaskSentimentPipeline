d5552eac076e9badec8e06c874c5d433b67c87f5
Joshua Wacks <2143116@students.wits.ac.za>
2023-10-22 19:00:52 +0200

    Created readme for task1 and completed task2

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # FlaskSentimentPipeline
+
+## This was run with python version 3.10.9
+
+### Follow the steps below to run both task1 and task2
+1. To ensure libraries are of their correct versions, first run the following command in the same folder as the files:
+	pip install -r requirements.txt
+2. To run the endpoint that will receive post requests, run the following command:
+	python endpoint.py
+3. After running the previous command, you should see the following output in the terminal:
+	![Image of endpoint being run successfully ](/endpointUp.png)
+
+	If you do not see the following image, ensure that the versions of your libraries match that in the requirements.txt
+4. Open an API platform such as Postman, which will allow you to test the endpoint.
+5. Create a new post request with the following URL: http://127.0.0.1:5000/
+6. The endpoint takes the following parameters in the request body:
+	- text: The text that the sentiment analysis should be performed on.
+	- safe_request(Boolean): If the text should be checked to contain any profanity and refused if it does.
+	- remove_stop_words(Boolean): To remove stop words as part of the preprocessing pipeline.
+	- normalize_words(Boolean): To normalize all the words, using lemming, as part of the preprocessing pipeline.
+	- remove_noise(Boolean): To remove the noise in the text, for now it just removes punctuation, as part of the preprocessing pipeline.
+7. If the request is valid the response will be in the following form:
+	- valid_input: If the input was valid or not.
+	- reason: If the input was not valid, why was it not, if it was it'll say "NA".
+	- sentiment: Which contains  both the label and the score or confidence in that label.
+
--- endpoint.py
+++ endpoint.py
@@ -1,8 +1,13 @@
 from flask import Flask, jsonify, request 
 from flask_restful import Resource, Api 
 from transformers import pipeline
+
 from profanity_check import predict, predict_prob
-import json
+import json,string
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
 
 app = Flask(__name__) 
 api = Api(app)
@@ -10,33 +15,67 @@ api = Api(app)
 # TODO: Some Ideas for improvemts
 # Allow for different models, based on different params
 # Return all model results based on params
-# Preprocessing
+
 
 # Would be cool to dockerize
 # Host endpoint on free hosting service or Azure?
 
-TODO: Bigram and Trigram tokenization in task2
+# TODO: Bigram and Trigram tokenization in task2
 
 class endpoint(Resource):
 	def __init__(self):
 		self.classification = pipeline('sentiment-analysis',model="distilbert-base-uncased-finetuned-sst-2-english")
         
-	def get(self):
-		return jsonify({'message': 'I\'m up'}) 
+	def preprocess_text(self,text, remove_stop_words = False, normalize_words = False, remove_noise = False):
+		corpus = word_tokenize(text)
+		tokens = pos_tag(corpus)
+
+		stop_words = stopwords.words('english')
+		lemmatizer = WordNetLemmatizer()
+	
+		result_tokens = []
+
+		for token,tag in tokens:
+      
+			#First check that we should be removing stop words and if so, check if this is a stop word
+			if remove_stop_words and token in stop_words:
+				continue
+		
+			if normalize_words:
+				if tag.startswith("NN"):
+					pos = 'n'
+				elif tag.startswith('VB'):
+					pos = 'v'
+				else:
+					pos = 'a'
+				token = lemmatizer.lemmatize(token, pos)
+	
+			if remove_noise and token in string.punctuation: # Noise in the text may need to be removed depending on the problem domain, here punctuation is the noise that is being removed
+				continue
+		
+			result_tokens.append(token)
+
+		joined_text = ' '.join(result_tokens)
+		return joined_text
 
 	def post(self):
 		
 		data = request.data
 		data = json.loads(data)
 		input_text = data["text"]
-		prob_contains_profanity = predict_prob([input_text])
-  
-		if prob_contains_profanity > 0.9:
-			response = jsonify({'valid_input':'No','reason':'Input contains profanity','sentiment': 'NA'})
+		if data["safe_request"]:
+			
+			prob_contains_profanity = predict_prob([input_text])
+			if prob_contains_profanity > 0.9:
+				response = jsonify({'valid_input':'No','reason':'Input contains profanity','sentiment': 'NA'})
+				response.status_code = 200
+				return response
+
 		else:
-			sentiment = self.classification(input_text)
-			response = jsonify({'valid_input':'Yes','reason':'NA','sentiment': sentiment})
-   
+			preprocessed_text = self.preprocess_text(input_text,data["remove_stop_words"])
+			sentiment = self.classification(preprocessed_text)
+			response = jsonify({'valid_input':'Yes','reason':'NA','sentiment': sentiment[0]})
+		print(sentiment[0])
 		response.status_code = 200
 		return response
 
